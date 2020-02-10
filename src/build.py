13a5c783f73dab2781e09453efd5520eb9635ee0
gbrls <gabrielschneider100@protonmail.com>
2020-02-09 21:57:25 -0300

    automated commit

--- src/build.py
+++ src/build.py
@@ -1,3 +1,7 @@
 import os
-# todo
-pass
+
+os.system('rm *.html')
+os.system('python main.py')
+os.system('git add .')
+os.system('''git commit -m "automated commit"''')
+os.system('git push origin master')
